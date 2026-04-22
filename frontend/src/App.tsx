2ac44c2d0f203ab570c070bd6ca6c32460d433ae
ShiirooS <shiiroo1808@gmail.com>
2026-04-22 00:23:05 -0500

    feature: Correccion de rutas de UI (Landing) y configuracion varialbe de Clerk

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './pages/Home';
 import Login from './pages/Login';
+import Landing from './pages/Landing';
 
 function App() {
   return (
@@ -9,7 +10,8 @@ function App() {
       <Header />
       <main>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Landing />} />
+          <Route path="/home" element={<Home />} />
           <Route path="/login" element={<Login />} />
         </Routes>
       </main>
